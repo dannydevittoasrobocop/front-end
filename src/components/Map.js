798032f310e706bdaa2b50c49f25d2af31097081
Michael <skaman4459@gmail.com>
2020-04-29 17:31:17 -0400

    a

--- src/components/Map.js
+++ src/components/Map.js
@@ -19,11 +19,56 @@ const Map = () => {
 
     // <rect x="0" y="0" rx="20" ry="20" width="15" height="15"/>
 
+    const circle = () => {
+        return (               
+            <rect x="0" y="0" rx="20" ry="20" width="15" height="15"/>
+        )
+    }
+
     return(
         <div>
-            <svg id='map'>
-                <rect x="0" y="0" rx="20" ry="20" width="15" height="15"/>
-            </svg>
+            {/* <svg id='map'>                 */}
+                {/* <rect x="0" y="0" rx="20" ry="20" width="15" height="15"/> */}
+
+                {roomMap.map(i => {
+                    return (
+                        <div style={{border: '1px solid red', height: "30px"}}>
+                            
+                        </div>
+                    )
+                })}
+               
+
+                {/* {roomMap.map(i => {
+                        console.log(roomMap)
+
+                        
+
+                        if (i.e_to > 0) {
+                            console.log(i.title,'it\'s a  direction', i.e_to)
+                            circle()                     
+                        }
+                        else if (i.w_to > 0) {
+                            console.log(i.title,'it\'s a  direction',i.w_to)
+                            circle()                     
+
+                        }
+                        else if (i.s_to > 0) {
+                            console.log(i.title, 'it\'s a  direction', i.s_to)
+                            circle()                     
+
+                        }
+                        else if (i.n_to > 0) {
+                            console.log(i.title,'it\'s a  direction', i.n_to)
+                            circle()                     
+
+                        }       
+                                
+                })} */}
+            {/* <svg id='map'>                
+            <rect x="0" y="0" rx="20" ry="20" width="15" height="15"/>
+            </svg> */}
+            {/* </svg>  */}
         </div>
     )
 }
